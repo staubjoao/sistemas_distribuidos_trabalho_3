17c746badf2fe910dae2ed32cbd1b13a341acfa2
yonix42 <joao.staub42@gmail.com>
2024-03-11 14:34:34 -0300

    Adicionando a config

---
+++ configuracao/src/main/java/br/uem/configuracao/ConfiguracaoApplication.java
@@ -0,0 +1,15 @@
+package br.uem.configuracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfiguracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguracaoApplication.class, args);
+	}
+
+}
---
+++ configuracao/src/test/java/br/uem/configuracao/ConfiguracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uem.configuracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
